42a6db64b3cdd76aeb878ecd479ef24bb3f52712
Francisco Knebel <franciscopaivaknebel@gmail.com>
2019-05-03 19:56:04 -0300

    Chalk and tmp dir generation.

--- cli.js
+++ cli.js
@@ -1,34 +1,41 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
+const chalk = require('chalk');
 const shelljs = require('shelljs');
 function shell(command) {
   return shelljs.exec(command);
 }
+function log(message, ...optionalParams) {
+  if (optionalParams.length > 0) {
+    return console.log(message, optionalParams);
+  } else {
+    return console.log(message);
+  }
+}
 
-const apps = process.env.HEROKU_APPS.split(",");
+const tmp = require('tmp');
+tmp.setGracefulCleanup();
 
-shelljs.mkdir('temp');
-shelljs.cd('temp');
+const temp = tmp.dirSync({ unsafeCleanup: true });
+const tempPath = temp.name;
+const cleanup = temp.removeCallback;
 
+shelljs.cd(tempPath);
+
+const apps = process.env.HEROKU_APPS.split(',');
 const tasks = apps.map((app) => {
   return new Promise(resolve => {
-    function cleanup() {
-      shelljs.cd('..');
-      shelljs.rm('-rf', app);
-    }
-
-    console.log(`Setting stack for ${app}`);
+    log(`Setting stack for ${chalk.red.bold(app)}`);
     shell(`heroku stack:set heroku-18 -a ${app}`);
     let err = shell(`heroku git:clone -a ${app}`).stderr;
     shelljs.cd(app);
 
     if(err && err.length > 0) {
       if (err.includes('warning: You appear to have cloned an empty repository.')) {
-        cleanup();
         const response = `${app}: Empty repository. GitHub autodeploy? Check README.`;
         
-        console.log(response);
+        log(chalk.yellow.bold(response));
         return resolve(response);
       }
     }
@@ -36,14 +43,14 @@ const tasks = apps.map((app) => {
     shelljs.cd(app);
     shell(`git commit --allow-empty -m "Upgrading ${app} to heroku-18"`);
     shell('git push heroku master');
+    shelljs.cd(tempPath);
 
-    cleanup();
     return resolve(`${app}: Success!`);
   });
 });
 
 Promise.all(tasks).then(result => {
-  console.log('DONE!', result);
+  log('DONE!', result);
   shelljs.cd('..');
-  shelljs.rm('-rf', 'temp');
+  cleanup();
 });
